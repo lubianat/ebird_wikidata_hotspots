484683e50f2980b99a94038ac02c278256776a0e
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-05-28 09:50:21 -0300

    Add distance parameter to the app

--- app.py
+++ app.py
@@ -6,46 +6,55 @@ app = Flask(__name__)
 
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
+
 def run_sparql_query(locIDs):
-    locIDs_values = ' '.join(f'"{id}"' for id in locIDs)
-    sparql.setQuery(f"""
+    locIDs_values = " ".join(f'"{id}"' for id in locIDs)
+    sparql.setQuery(
+        f"""
         SELECT ?item ?locId
         WHERE 
         {{
             VALUES ?locId {{ {locIDs_values} }}
             ?item wdt:P5200 ?locId.
         }}
-    """)
+    """
+    )
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     return results["results"]["bindings"]
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/map', methods=['GET'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/map", methods=["GET"])
 def map():
-    lat = request.args.get('lat', '')
-    lng = request.args.get('lng', '')
-    ebird_api_url = f"https://api.ebird.org/v2/ref/hotspot/geo?lat={lat}&lng={lng}&fmt=json"
+    lat = request.args.get("lat", "")
+    lng = request.args.get("lng", "")
+    dist = request.args.get("dist", "25")
+    ebird_api_url = f"https://api.ebird.org/v2/ref/hotspot/geo?lat={lat}&lng={lng}&dist={dist}&fmt=json"
     response = requests.get(ebird_api_url)
     data = response.json()
 
-    locIDs = [item['locId'] for item in data]
+    locIDs = [item["locId"] for item in data]
     wikidata_results = run_sparql_query(locIDs)
 
     # create a dictionary for easier access
-    wikidata_dict = {result['locId']['value']: result['item']['value'] for result in wikidata_results}
+    wikidata_dict = {
+        result["locId"]["value"]: result["item"]["value"] for result in wikidata_results
+    }
 
     # iterate through the data to add wikidata information
     for item in data:
-        if item['locId'] in wikidata_dict:
-            item['wikidata'] = wikidata_dict[item['locId']]
+        if item["locId"] in wikidata_dict:
+            item["wikidata"] = wikidata_dict[item["locId"]]
         else:
-            item['wikidata'] = None
+            item["wikidata"] = None
 
-    return render_template('map.html', data=data, lat=lat, lng=lng)
+    return render_template("map.html", data=data, lat=lat, lng=lng)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
