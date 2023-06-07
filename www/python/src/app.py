4a7dee577c66dc48d8226d0ccc60f985ec483a22
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-06-07 10:19:41 -0300

    Add basic error handling

--- www/python/src/app.py
+++ www/python/src/app.py
@@ -40,6 +40,16 @@ def home():
     return render_template("index.html")
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
+
+
 @app.route("/map", methods=["GET"])
 def map():
     lat = request.args.get("lat", "")
